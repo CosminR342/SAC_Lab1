721bac82b90ef3cbb70b3e72f04b2161c6c88d73
CosminR342 <147655946+CosminR342@users.noreply.github.com>
2023-10-29 21:00:22 +0200

    Added recommendations from users

--- sac_lab1.py
+++ sac_lab1.py
@@ -1,13 +1,17 @@
 from recombee_api_client.api_client import RecombeeClient, Region
-from recombee_api_client.api_requests import AddItem, AddItemProperty, SetItemValues, ListItems
+from recombee_api_client.api_requests import AddItem, AddItemProperty, SetItemValues, ListItems, AddUser, AddUserProperty, RecommendItemsToUser, SetUserValues, AddRating
 from recombee_api_client.exceptions import APIException
 import pandas as pd
+import uuid
+import random
 
 client = RecombeeClient(
   'upb-sac-dev', 
   'zmBSrKrHi0IImfXjbJyOFGaA5tjNZR2MdY0d7YtQX559zQlfY7gDjeN4MCG5QDnR', 
   region=Region.EU_WEST
 )
+
+MOVIES_NR = 20;
 
 df = pd.read_csv('./archive/top250.csv', usecols=["name", "rating", "genre", "year"])
 
@@ -51,4 +55,29 @@ def printItems():
     result = client.send(ListItems(return_properties=True))
     print(result)
 
-printItems()
+# Part 2 / Lab 3
+
+usernames = ["alin12", "mihnea93", "marian_ro1"]
+uuids = [uuid.uuid1(), uuid.uuid1(), uuid.uuid1()]
+
+def addUsers():
+    for i in range(0, 3):
+        client.send(AddUser(uuids[i]))
+        client.send(SetUserValues(uuids[i], {
+            "username": usernames[i]
+        }))
+
+def sendInteractionsAndReceiveRecommendation(userIndex):
+    for ind, movie in df.sample(n=MOVIES_NR).iterrows():
+        rating = random.uniform(-1.0, 1.0)
+        client.send(AddRating(str(uuids[userIndex]), str(ind), rating))
+        print("User " + usernames[userIndex] + " gave rating " + str(rating) + " for " + movie['name'])
+    recommandations = client.send(RecommendItemsToUser(str(uuids[userIndex]), MOVIES_NR))
+    print("Recommendations: ", recommandations)
+
+def addUsernameProperty():
+    client.send(AddUserProperty("username", "string"))
+
+addUsers()
+for i in range(0, 3):
+    sendInteractionsAndReceiveRecommendation(i)
